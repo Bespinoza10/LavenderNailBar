11efaf093c5efbbf54f25732e899cdcc4dd1c3b6
Brandon Espinoza <espinozab100@gmail.com>
2017-08-16 15:34:14 -0500

    Created Testimonial Model and Controller. WIP in markup and form.

---
+++ app/controllers/testimonials_controller.rb
@@ -0,0 +1,46 @@
+class TestimonialsController < ApplicationController
+
+  def index
+    @testimonial = Testimonial.all
+  end
+
+  def new
+    @testimonial = Testimonial.new
+  end
+
+  def create
+    @testimonial = Testimonial.new testimonial_params
+
+    if @testimonial.save
+      redirect_to root_path, notice: "Your Testimonial has been saved."
+    else
+      render 'new', notice: "E R R O R !!!  Please try again. If not contact Brandon at 615-337-6981"
+    end
+  end
+
+  def show
+  end
+
+  def update
+    if @testimonial.update testimonial_params
+      redirect_to @root_path, notice: "You have updated your testimonial from #{@testimonial.name}"
+    else
+      render 'edit'
+    end
+  
+    def destroy
+      @testimonial.destroy
+      redirect_to root_path
+    end
+
+  private
+
+  def testimonial_params
+    params.require(:testimonial).permit(:name, :description)
+  end
+
+  def find_testimonial
+    @testimonial = Testimonial.find(params[:id])
+  end
+
+end
