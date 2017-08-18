71d12bfabae588a0b0f2f88731ab821dcd361ad2
Brandon Espinoza <espinozab100@gmail.com>
2017-08-18 11:00:18 -0500

    Redirect to root_path if contact message was succesfully

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,6 +8,7 @@ class ContactsController < ApplicationController
     @contact.request = request
     if @contact.deliver
       flash.now[:error] = nil
+      redirect_to root_path, notice: "Your Message has been delivered. We will get in touch with you as soon as possible."
     else
       flash.now[:error] = 'Sorry Cannot send message.'
       render :new
